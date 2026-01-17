786ae311cca900f2f6852acb619fbcbf7b438d5c
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2026-01-17 14:50:57 -0500

    Add reset sh.

---
+++ reset.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+curl 'https://raw.githubusercontent.com/yspreen/ralph/refs/heads/main/entry.md' > entry.md
+curl 'https://raw.githubusercontent.com/yspreen/ralph/refs/heads/main/progress.txt' > progress.txt
+git rm step-*.md done.txt
+git add entry.md progress.txt
+git commit -m "Run a ralph reset."
+
+echo "Now update userinput.txt and run ralph.sh again."
