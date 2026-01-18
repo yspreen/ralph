248241d14f98f294a34cf56e64c8fa977225e685
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2026-01-17 19:49:49 -0500

    Include blocker in reset.

--- reset.sh
+++ reset.sh
@@ -2,7 +2,7 @@
 
 curl 'https://raw.githubusercontent.com/yspreen/ralph/refs/heads/main/entry.md' > entry.md
 curl 'https://raw.githubusercontent.com/yspreen/ralph/refs/heads/main/progress.txt' > progress.txt
-git rm step-*.md done.txt
+git rm step-*.md done.txt blocker.md
 git add entry.md progress.txt
 git commit -m "Run a ralph reset."
 
