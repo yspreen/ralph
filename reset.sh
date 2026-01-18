30cf604ab5fb5176032edf9d673532fb92a62afe
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2026-01-17 21:08:04 -0500

    Update reset.

--- reset.sh
+++ reset.sh
@@ -2,6 +2,7 @@
 
 curl 'https://raw.githubusercontent.com/yspreen/ralph/refs/heads/main/entry.md' > entry.md
 curl 'https://raw.githubusercontent.com/yspreen/ralph/refs/heads/main/progress.txt' > progress.txt
+curl 'https://raw.githubusercontent.com/yspreen/ralph/refs/heads/main/step-1.md' > step-1.md
 git rm step-*.md done.txt blocker.md
 git add entry.md progress.txt
 git commit -m "Run a ralph reset."
